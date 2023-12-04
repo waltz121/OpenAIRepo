44513117bac9860f82fee41425e30c80ab410559
Walter Robbin Robledo <walterr@newmercola.com>
2023-12-04 13:41:13 +0800

    Added ViewModel and Created Partial view to Accomodate it

---
+++ ChatBot/Models/ViewModels/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatBot.Models.ViewModels
+{
+    public class ChatViewModel
+    {
+        private string content {  get; set; }
+
+        public ChatViewModel() {
+            Content = "<div class=\"messages\"><div class=\"messages-content\"></div></div>";
+        }
+        public string Content { get; set; }
+        public string TxtMessage { get; set; }
+    }
+}
