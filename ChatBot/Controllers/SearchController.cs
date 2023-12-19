c416e5c1bfbc1fffea83a1b86a6660eb384304fc
walterr <walterr@MNL-WALTERR>
2023-12-19 09:21:51 +0800

    Initial Search Page

---
+++ ChatBot/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
