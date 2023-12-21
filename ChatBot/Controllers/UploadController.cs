312de2908697ccc7735b57ccbfa12206bd2bce2c
walterr <walterr@MNL-WALTERR>
2023-12-21 09:46:12 +0800

    Initial Code setup for view and controllers.

---
+++ ChatBot/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using ChatBot.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            UploadViewModel vm = new UploadViewModel();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UploadViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                // Do something
+                vm.SaveUrl();
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ ChatBot/Models/ViewModels/UploadViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+namespace ChatBot.Models.ViewModels
+{
+    public class UploadViewModel
+    {
+        public UploadViewModel()
+        {
+
+        }
+
+        public UploadViewModel(string url, string resultMessage)
+        {
+            this.url = url;
+            this.resultMessage = resultMessage;
+        }
+
+        private string url;
+        private string resultMessage;
+
+        public string Url { get => url; set => url = value; }
+        public string ResultMessage { get => resultMessage; set => resultMessage = value; }
+
+        public void SaveUrl()
+        {
+            // Code for getting the Html from a url link.
+                        
+
+        }
+    }
+}
